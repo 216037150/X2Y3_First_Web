47820fde649e7488bc8674b552faa1fafa0f302d
Christiaan C Cupido <157732548+CCupido24@users.noreply.github.com>
2024-03-14 10:36:14 +0200

    logi button

--- script.js
+++ script.js
@@ -18,35 +18,35 @@ function submitForm(formId) {
     }
 }
 
-// function for sliding images (Courses on our front page)
+// // function for sliding images (Courses on our front page)
 
-document.addEventListener("DOMContentLoaded", function () {
-    const slides = document.querySelector('.services');
-    const slideWidth = slides.firstElementChild.clientWidth;
-    let currentSlide = 0;
-    let intervalId;
+// document.addEventListener("DOMContentLoaded", function () {
+//     const slides = document.querySelector('.services');
+//     const slideWidth = slides.firstElementChild.clientWidth;
+//     let currentSlide = 0;
+//     let intervalId;
   
-    function nextSlide() {
-      currentSlide = (currentSlide + 1) % slides.children.length;
-      slides.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-    }
+//     function nextSlide() {
+//       currentSlide = (currentSlide + 1) % slides.children.length;
+//       slides.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+//     }
   
-    function startSlider() {
-      intervalId = setInterval(nextSlide, 450);
-    }
+//     function startSlider() {
+//       intervalId = setInterval(nextSlide, 450);
+//     }
   
-    function stopSlider() {
-      clearInterval(intervalId);
-    }
+//     function stopSlider() {
+//       clearInterval(intervalId);
+//     }
   
-    // Start the slider initially
-    startSlider(); 
+//     // Start the slider initially
+//     startSlider(); 
   
-    // Stop slider on mouse enter
-    // Restart slider on mouse leave
-    slides.addEventListener('mouseenter', stopSlider); 
-    slides.addEventListener('mouseleave', startSlider); 
-  });
+//     // Stop slider on mouse enter
+//     // Restart slider on mouse leave
+//     slides.addEventListener('mouseenter', stopSlider); 
+//     slides.addEventListener('mouseleave', startSlider); 
+//   });
   document.addEventListener("DOMContentLoaded", function () {
   const slides = document.querySelector('.slides');
   const slideWidth = slides.firstElementChild.clientWidth;
